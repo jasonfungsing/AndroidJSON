656ae7f88d744fd932b856f40832c38e444f2300
Jason Fung <jasonfungsing@gmail.com>
2012-01-19 23:21:21 +1100

    Changed code to fetch twitter feed.

--- AndroidJSONTwitter/src/com/jasonfc/AndroidJSONTwitterActivity.java
+++ AndroidJSONTwitter/src/com/jasonfc/AndroidJSONTwitterActivity.java
@@ -1,13 +1,80 @@
 package com.jasonfc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class AndroidJSONTwitterActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		String readTwitterFeed = readTwitterFeed();
+		
+		StringBuilder page = new StringBuilder();
+		try {
+			JSONArray jsonArray = new JSONArray(readTwitterFeed);
+			Log.i(AndroidJSONTwitterActivity.class.toString(), "Number of entries " + jsonArray.length());
+			for (int i = 0; i < jsonArray.length(); i++) {
+				JSONObject jsonObject = jsonArray.getJSONObject(i);
+				page.append(jsonObject.getString("text"));
+				page.append("\n");
+				Log.i(AndroidJSONTwitterActivity.class.toString(), jsonObject.getString("text"));
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		TextView vw1 = (TextView) findViewById(R.id.text_view_1);
+		vw1.setText(page.toString());
+
+	}
+
+	private String readTwitterFeed() {
+		StringBuilder builder = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet httpget = new HttpGet("https://twitter.com/statuses/user_timeline/jasonfungsing.json");
+
+		try {
+			HttpResponse response = client.execute(httpget);
+			StatusLine statusline = response.getStatusLine();
+			int statusCode = statusline.getStatusCode();
+			if (statusCode == 200) {
+				HttpEntity entity = response.getEntity();
+				InputStream content = entity.getContent();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					builder.append(line);
+				}
+			} else {
+				Log.e(AndroidJSONTwitterActivity.class.toString(), "Failed to download file");
+			}
+
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return builder.toString();
+	}
 }
