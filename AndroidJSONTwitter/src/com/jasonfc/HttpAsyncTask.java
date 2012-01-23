ed6a5450b8aeb7bf1df7ad4d441ddca22efdf204
Jason Fung <jasonfungsing@gmail.com>
2012-01-23 23:25:23 +1100

    Add toast message when io exception on connection error

--- AndroidJSONTwitter/src/com/jasonfc/HttpAsyncTask.java
+++ AndroidJSONTwitter/src/com/jasonfc/HttpAsyncTask.java
@@ -11,11 +11,15 @@ import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.params.ConnManagerPNames;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
 
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
 
 public class HttpAsyncTask extends AsyncTask<String, Integer, String> {
 
@@ -23,19 +27,20 @@ public class HttpAsyncTask extends AsyncTask<String, Integer, String> {
 	private String result;
 	private AndroidJSONTwitterActivity ajt;
 	private ProgressDialog dialog;
-	
+
 	public HttpAsyncTask(String url, String result, AndroidJSONTwitterActivity ajt) {
 		this.url = url;
 		this.result = result;
-		this.ajt=ajt;
+		this.ajt = ajt;
 	}
-	
-	
+
 	@Override
 	protected String doInBackground(String... arg0) {
 
 		StringBuilder builder = new StringBuilder();
-		HttpClient client = new DefaultHttpClient();
+		HttpParams httpParameters = new BasicHttpParams();
+		httpParameters.setIntParameter(ConnManagerPNames.MAX_TOTAL_CONNECTIONS, 5000);
+		HttpClient client = new DefaultHttpClient(httpParameters);
 		HttpGet httpget = new HttpGet(url);
 		try {
 			HttpResponse response = client.execute(httpget);
@@ -56,22 +61,29 @@ public class HttpAsyncTask extends AsyncTask<String, Integer, String> {
 		} catch (ClientProtocolException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
+			Log.e(AndroidJSONTwitterActivity.class.toString(), "Http Async task Io exception");
+			publishProgress(1);
 			e.printStackTrace();
 		}
 		return builder.toString();
 	}
-	
+
 	@Override
 	protected void onPreExecute() {
 		dialog = new ProgressDialog(ajt, ProgressDialog.STYLE_SPINNER);
 		dialog.setTitle("JSON Twitter");
-        dialog.setMessage("Please, wait...");
-        dialog.show();
+		dialog.setMessage("Please, wait...");
+		dialog.show();
 	}
+	
+	@Override
+	protected void onProgressUpdate(Integer... progress) {
+		Toast.makeText(ajt, "Can not conntection to Internet, try later",Toast.LENGTH_LONG).show();
+	}
+
 	@Override
 	protected void onPostExecute(String result) {
 		dialog.dismiss();
 		ajt.getResponse(result);
 	}
-
 }
